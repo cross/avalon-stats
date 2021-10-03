aeb23eb79d9751f16fe540a457fe67d7358a4e88
Chris Ross <cross+github@distal.com>
2021-10-03 10:00:55 -0400

    First pass at assembling data for graphite.  Right now only dumps it to stdout.

--- cgstats.py
+++ cgstats.py
@@ -8,6 +8,7 @@ import sys
 import argparse
 import re
 from collections import OrderedDict
+import time
 from datetime import timedelta
 from pprint import pprint
 
@@ -101,40 +102,87 @@ def api_get_summary(server=args.server,port=args.port):
 def api_get_stats(server=args.server,port=args.port):
     return _api_command("stats",None,server,port)
 
-response = api_get_summary()
-respdata = handle_response(response,"summary") # Will exit on failure, return summary dict on success
-
+# TODO: Should make an argparser for this
 if args.graphite:
-    print("TODO:  Should be outputting graphite data, but don't know how to yet!")
+    # Parse the argument, which is expected to be a host:port specification
+    # (but, other things also allowed for unusual run modes)
+    # TODO: This gets things wrong somtimes.  Should likely improve it.
+    hostportpat = re.compile(r'(\d+\.\d+\.\d+\.\d+|\[(?:[0-9a-fA-F]+)?:?(?:\:[0-9a-fA-F]*)+\]|[\w\-_]+(?:\:[\w\-_]+)*):(\d+)')
+    m = hostportpat.match(args.graphite)
+    if m:
+        hostspec = m.group(1)
+        if hostspec[0] == '[' and hostspec[-1] == ']':
+            hostspec = hostspec[1:-1]
+        port = int(m.group(2))
+        print("Got host spec {}, port {}, for graphite server".format(hostspec,port))
+    elif args.graphite == "-":
+        hostspec = None
+        print("Should output graphite data to stdout")
+    else:
+        print("Got non host:port value {} for graphite server".format(args.graphite))
+        sys.exit(6)
 
+
+response = api_get_summary()
+now = int(time.time())
+respdata = handle_response(response,"summary") # Will exit on failure, return summary dict on success
 #pprint(respdata)
-print("Summary:")
-print("  Running for: {}".format(timedelta(seconds=respdata['Elapsed'])))
-print("  GHS av     : {:7.2f}".format(respdata['MHS av']/1024.0))
-print("  Accepted   : {:7d}".format(respdata['Accepted']))
-print("  Rejected   : {:7d}".format(respdata['Rejected']))
+
+prefix = 'collectd.crosstest'
+if args.graphite:
+    sectprefix = prefix + '.summary'
+    records = [ ('{}.elapsed'.format(sectprefix),(now,int(respdata['Elapsed']))),
+                ('{}.accepted'.format(sectprefix),(now,int(respdata['Accepted']))),
+                ('{}.rejected'.format(sectprefix),(now,int(respdata['Rejected']))),
+              ]
+    for k,v in [(x,respdata[x]) for x in respdata.keys() if x[0:3] == "MHS"]:
+        records.append(('{}.{}'.format(sectprefix,".".join(k.split())).lower(),(now,int(v))))
+else:
+    #pprint(respdata)
+    print("Summary:")
+    print("  Running for: {}".format(timedelta(seconds=respdata['Elapsed'])))
+    print("  GHS av     : {:7.2f}".format(respdata['MHS av']/1024.0))
+    print("  Accepted   : {:7d}".format(respdata['Accepted']))
+    print("  Rejected   : {:7d}".format(respdata['Rejected']))
 
 # TODO: Print pool/work information?
 
 # Print per-device stats
 response = api_get_stats()
+now = int(time.time())
 respdata = handle_response(response,"stats") # Will exit on failure, return stats list on success
 #pprint(respdata)
 
 (stats0,stats1) = response['STATS']
-
 stats0 = restructure_stats0(stats0)
 #pprint(stats0)
-print("Device stats:")
+
+prefix = 'collectd.crosstest'
+if not args.graphite:
+    print("Device stats:")
+#else:
+#    records = []
+
 for i in stats0['MM']:
-    print("MM {:4s}: {:>10s}  {}/{}".format(i['DNA'][-4:],"Ghz (?/5m)",i['GHSmm'],i['GHS5m']))
-    print("{:7}: {:>10s}  {}rpm".format("","Fan",i['Fan']))
+    if args.graphite:
+        sectprefix = prefix + '.stats.mm.{}'.format(i['DNA'])
+        records.append(('{}.fan'.format(sectprefix),(now,int(i['Fan']))))
+        records.append(('{}.ghsmm'.format(sectprefix),(now,float(i['GHSmm']))))
+        records.append(('{}.ghs5m'.format(sectprefix),(now,float(i['GHS5m']))))
+    else:
+        print("MM {:4s}: {:>10s}  {}/{}".format(i['DNA'][-4:],"Ghz (?/5m)",i['GHSmm'],i['GHS5m']))
+        print("{:7}: {:>10s}  {}rpm".format("","Fan",i['Fan']))
     # I'm not sure what 'Temp' 'Temp0' and 'Temp1' each are, but 'Temp' looks
     # to be much lower, so maybe enclosure?  I'm going to average 'Temp0' and
     # 'Temp1' to be the number I think I'm looking for, but that's just a
     # random guess.
     temp = (float(i['Temp0'])+float(i['Temp1']))/2.0;
-    print("{:7}: {:>10s}  {:.1f}°C".format("","Temp",temp))
-#elif 'STATUS' in response:
+    if args.graphite:
+        records.append(('{}.temp'.format(sectprefix),(now,int(i['Temp']))))
+        records.append(('{}.temp0'.format(sectprefix),(now,int(i['Temp0']))))
+        records.append(('{}.temp1'.format(sectprefix),(now,int(i['Temp1']))))
+    else:
+        print("{:7}: {:>10s}  {:.1f}°C".format("","Temp",temp))
 
-
+if args.graphite:
+    pprint(records)

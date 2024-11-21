0069a008ff5554c3a0ed83558e25505511ce2ea2
Chris Ross <cross+github@distal.com>
2024-11-21 09:49:45 -0500

    Implement on/off, fix a bug in the status parser, and remove a bunch of the code from the original application that is not used or needed.

--- synaccess.py
+++ synaccess.py
@@ -87,9 +87,11 @@ def get_status(sess):
         # list[::-1] walks the whole list backwards
         #pprint(list(states)[::-1])
         #pprint(dict(enumerate(list(states)[::-1])))
-        #retval['outlet_state'] =  { i: bool(v) for i,v in dict(enumerate(list(states)[::-1], 1)) }
-        for i,v in enumerate(list(states)[::-1]):
-            retval['outlet_state'][i] = bool(v)
+        # nb: Be cautions here.  bool('0') is True if it's a string.
+        retval['outlet_state'] = { i: bool(int(v)) for i,v in enumerate(list(states)[::-1]) }
+#        for i,v in enumerate(list(states)[::-1]):
+#            pprint([v, bool(v)])
+#            retval['outlet_state'][i] = bool(v)
 
         retval['current'] = float(resp[2])
         # Docs say there can be one or two current-draw numbers.  My unit has
@@ -134,10 +136,8 @@ parser.add_argument('-s','--server','--host', default='127.0.0.1', help='API ser
 parser.add_argument('-p','--port', type=int, default=80, help='API server port')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('--status', action='store_true', help='show outlet status')
-#monitor = group.add_argument_group()
-#monitor.add_argument('-m','--monitor', action='store_true', help='Begin monitoring mode (does not exit)', required=True)
-#monitor.add_argument('-l','--log', type=argparse.FileType('w'), help='log machine-readable data to FILE')
-# argparse doesn't cope with nested groups, at least not well enough.
+# Shame we can't have another group here, for monitor and log, but argparse
+# doesn't cope with nested groups, at least not well enough.
 group.add_argument('-m','--monitor', action='store_true', help='Begin monitoring mode (does not exit)', )
 parser.add_argument('-l','--log', type=argparse.FileType('w'), help='log machine-readable data to FILE')
 group.add_argument('--on', action='store_true', help='Turn the outlet group on')
@@ -166,12 +166,36 @@ if args.status:
         print("Outlet #{} is {}".format(i+1,"on" if v else "off"))
     if 'temp' in data:
         print("Temperature is {}°C".format(data['temp']))
+    if 'current' in data:
+        print("Current draw is {}A".format(data['current']))
 
     sys.exit(0)
 
 if args.on or args.off:
-    print("This opertation is not yet supported.  Try something else.")
-    sys.exit(1)
+    # These commands come from the WebAPI, and are not the same as the
+    # coded commands listed in the HTTP API documentation at
+    # https://static1.squarespace.com/static/54d27fb4e4b024eccdd9e569/t/651d79e2e2de6d3883208352/1696430563393/1094_NPStartup_V20.pdf
+    # I don't know why that doc lists only a small subset, and with an
+    # alternate style, but these ones I've scraped from the device U/I
+    # work just fine.
+    if args.on:
+        cmd={ 'grp': 0 }
+        reqstate="on"
+    else:
+        cmd={ 'grp': 30 }
+        reqstate="off"
+    r = pdu.get('cmd.cgi', params=cmd)
+    if (r.status_code / 100) != 2:
+        print(f"Error.  Failed to set group power, HTTP status code {r.status_code}")
+        pprint(r.text)
+        sys.exit(1)
+    #pprint(r.text)
+    resp = r.text.strip()
+    #pprint(resp)
+    if resp == '$A0':
+        print(f"Outlet group has been powered {reqstate}.")
+    else:
+        print("Error.  Failed to retrieve status, API returned {} ({})".format(resp.text, str(resp)))
 
 if args.monitor:
     timefmt="%Y-%m-%d %T"
@@ -207,114 +231,9 @@ if args.monitor:
             print() # drop to new line after a ^C
             break
 
-
 sys.exit(0)
 
 now = int(time.time())
 if not response or 'result' not in response or response['result'] != "pong":
     raise RuntimeError("Unexpected response to miner_ping: {}".format(response))
 
-# Issue the real query to get stats
-miner.send_command("miner_getstatdetail")
-response = miner.get_resp()
-#pprint(response)   # The whole shebang of stat details
-if not response or 'result' not in response:
-    raise RuntimeError("Unexpected response to miner_getstatdetail: {}".format(response))
-
-respdata = handle_response(response) # Will exit on failure, return summary dict on success
-
-# XXX Lots more to fix (or remove)
-
-prefix = 'collectd.crosstest.gen2'
-if args.graphite:
-    sectprefix = prefix + '.summary'
-    records = [ ('{}.elapsed'.format(sectprefix),(now,int(respdata['Elapsed']))),
-                ('{}.accepted'.format(sectprefix),(now,int(respdata['Accepted']))),
-                ('{}.rejected'.format(sectprefix),(now,int(respdata['Rejected']))),
-              ]
-    for k,v in [(x,respdata[x]) for x in respdata.keys() if x[0:3] == "MHS"]:
-        records.append(('{}.{}'.format(sectprefix,".".join(k.split())).lower(),(now,int(v))))
-else:
-    #pprint(respdata)
-    print("Summary:")
-    print("  Running for: {:>8s}".format(str(timedelta(seconds=respdata['runtime']))))
-    if respdata['hashrate'] < 1100000:
-        print("  KHS av     : {:8.2f}".format(respdata['hashrate']/1000))
-    elif respdata['hashrate'] > 1100000000:
-        print("  GHS av     : {:8.2f}".format(respdata['hashrate']/1000/1000/1000))
-    else:
-        print("  MHS av     : {:8.2f}".format(respdata['hashrate']/1000/1000))
-    if respdata['shares'][0] > 0:
-        sharePerHour = respdata['shares'][0] / (respdata['runtime'] / 3600)
-        sharePerMin = respdata['shares'][0] / (respdata['runtime'] / 60)
-        if sharePerMin < 1 and sharePerHour > 1:
-            rateStr = "{:.2f} per hour".format(sharePerHour)
-        elif sharePerMin >= 1:
-            rateStr = "{:.2f} per minute".format(sharePerMin)
-        else: # sharePerHour <= 1
-            # For really slow cases, switch it to time/share
-            rateStr = "avg {} per share".format(humanize.naturaldelta(timedelta(seconds=int(respdata['runtime']/respdata['shares'][0]))))
-        print("  Accepted   : {:8d} ({})".format(respdata['shares'][0], rateStr))
-    else:
-        print("  Accepted   : {:8d}".format(respdata['shares'][0]))
-    print("  Rejected   : {:8d}".format(respdata['shares'][1]))
-    if respdata['shares'][0]:
-        print("  Last share : {:>8s} ago".format(str(timedelta(seconds=respdata['shares'][3]))))
-    else:
-        print("  Last share : {:>8s}".format("never"))
-
-# TODO: Print pool/work information?
-
-sys.exit(0);
-
-# Break-out and report per-device stats
-respdata = handle_response(response['stats'][0],"stats") # Will exit on failure, return stats list on success
-#pprint(respdata)
-
-(stats0,stats1) = respdata
-stats0 = restructure_stats0(stats0)
-#pprint(stats0)
-
-prefix = 'collectd.crosstest'
-if not args.graphite:
-    print("Device stats:")
-#else:
-#    records = []
-
-for i in stats0['MM']:
-    if args.graphite:
-        sectprefix = prefix + '.stats.mm.{}'.format(i['DNA'])
-        records.append(('{}.fan'.format(sectprefix),(now,int(i['Fan']))))
-        records.append(('{}.ghsmm'.format(sectprefix),(now,float(i['GHSmm']))))
-        records.append(('{}.ghs5m'.format(sectprefix),(now,float(i['GHS5m']))))
-    else:
-        print("MM {:4s}: {:>10s}  {}/{}".format(i['DNA'][-4:],"Ghz (?/5m)",i['GHSmm'],i['GHS5m']))
-        print("{:7}: {:>10s}  {}rpm".format("","Fan",i['Fan']))
-    # I'm not sure what 'Temp' 'Temp0' and 'Temp1' each are, but 'Temp' looks
-    # to be much lower, so maybe enclosure?  I'm going to average 'Temp0' and
-    # 'Temp1' to be the number I think I'm looking for, but that's just a
-    # random guess.
-    temp = (float(i['Temp0'])+float(i['Temp1']))/2.0;
-    if args.graphite:
-        records.append(('{}.temp'.format(sectprefix),(now,int(i['Temp']))))
-        records.append(('{}.temp0'.format(sectprefix),(now,int(i['Temp0']))))
-        records.append(('{}.temp1'.format(sectprefix),(now,int(i['Temp1']))))
-    else:
-        print("{:7}: {:>10s}  {:.1f}°C".format("","Temp",temp))
-
-if args.graphite:
-    if args.graphite == "-":
-        pprint(records)
-    else:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((hostspec,port))
-        sentbytes=0
-        payload = pickle.dumps(records,protocol=2)
-        message = struct.pack("!L", len(payload)) + payload
-        while sentbytes < len(message):
-            cnt = s.send(message[sentbytes:])
-            if (cnt == 0):
-                raise RuntimeError("socket connection broken")
-            sentbytes = sentbytes + cnt
-        s.close()
-        print("{}-byte message ({} bytes payload) sent to graphite server.".format(len(message),len(payload)))
